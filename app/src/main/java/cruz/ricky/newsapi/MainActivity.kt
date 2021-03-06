9cc05c1df6e57da84abc25edd7a7105dd4001048
Ricky Cruz <rczdev@gmail.com>
2021-03-05 18:18:00 -0600

    Add Fragments & Navigation Components

--- app/src/main/java/cruz/ricky/newsapi/MainActivity.kt
+++ app/src/main/java/cruz/ricky/newsapi/MainActivity.kt
@@ -2,10 +2,24 @@ package cruz.ricky.newsapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import cruz.ricky.newsapi.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavView.setupWithNavController(
+            nav_host_fragment.findNavController()
+        )
     }
+
 }
